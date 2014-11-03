16abd9e8f9f252a719f0353a43d4451932839d53
roma-turkin <roma.turkin@yahoo.com>
2014-11-03 23:42:01 +0300

    Nothing changed, but a bit of structure. Made the commit to make sure I'm on the right way.

--- JavaFiddle-war/web/resources/js/git.js
+++ JavaFiddle-war/web/resources/js/git.js
@@ -1,7 +1,24 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+function addFile(id) {
+    if(arguments.length === 0) {
+        id = getCurrentFileID();
+        addCurrentFileText();
+        $('#latest_update').text("Saving...");
+    }
+    
+    var time = new Date().getTime();
+    $.ajax({
+        url: PATH + '/webapi/git/file',
+        type:'POST', 
+        data: {id: id, value: getOpenedFileText(id)},
+        success: function() {
+            unModifiedTab(id);
+            addCurrentFileTimeStamp(time);
+            if (isCurrent(id))
+                $('#latest_update').text("All changes saved.");
+        },
+        error: function(jqXHR) {
+            if (jqXHR.status === 406)
+                $('#latest_update').text("Saving isn't acceptable.");
+        }
+    });
+}
